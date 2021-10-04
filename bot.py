a3d48511f189fb31b838108edab517032d418664
ClimateIsCreepy <85735927+ClimateIsCreepy@users.noreply.github.com>
2021-10-04 15:21:10 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,5 @@
-import os
-from typing import List
-import discord
-from discord import message
-from discord.errors import ClientException
 from discord.ext import commands
-import DiscordUtils
-from discord import Member
-import random
-from discord.ext.commands import context
-from discord.ext import commands
-from discord.ext.commands.bot import Bot 
+
  
 bot = commands.Bot(command_prefix='!')
 
